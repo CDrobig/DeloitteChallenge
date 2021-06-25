637777e27f9348c6122660f02f1ee6679ab92ae9
christina <christinadrobig@gmail.com>
2021-06-25 23:56:19 +0200

    add file upload and download

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, current_app, send_from_directory, send_file
 import pandas as pd
 import os
 from werkzeug.utils import secure_filename
@@ -31,10 +31,32 @@ def file_uploader():
         f.save(os.path.join("./", filename))
 
         df = pd.read_csv(filename, delimiter=",", encoding="utf-8")
-        #unbalancedColumns(df)
         checkData(df)
 
         return 'file uploaded successfully'
+
+@app.route('/uploadCV')
+def upload_CVfile():
+    return """<html>
+   <body>
+      <form action = "http://localhost:5000/CVuploader" method = "POST"
+         enctype = "multipart/form-data">
+         <input type = "file" name = "file" />
+         <input type = "submit"/>
+      </form>
+   </body>
+</html>"""
+
+@app.route('/CVuploader', methods=['GET', 'POST'])
+def CVfile_uploader():
+    if request.method == 'POST':
+        f = request.files['file']
+        filename = secure_filename(f.filename)
+        f.save(os.path.join("./exampleCV/original/", filename))
+        file = filename.split(".")[0]
+        #uploads = os.path.join(current_app.root_path, app.config['./exampleCV/unbiased'])
+        path = "./exampleCV/unbiased/"+file+"_unbiased.jpeg"
+        return send_file(path, as_attachment=True)
 
 
 
